54502f2f75b8531f356511e66e3762752f322b76
DeveloperAlex <developeralex@gmail.com>
2016-09-18 22:26:25 +0000

    Works on c9

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+
+/*
+> https://angular.io/docs/ts/latest/quickstart.html
+
+The QuickStart application doesn't do anything else, so you don't need any other modules. 
+In a real application, you'd likely import FormsModule as well as RouterModule and HttpModule. 
+These are introduced in the Tour of Heroes Tutorial.
+> https://angular.io/docs/ts/latest/tutorial/
+*/
+
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ AppComponent ],
